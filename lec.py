f26f2cc8339774deb2924670d331c334050af364
zbkr1p <zbkr1@rambler.ru>
2022-09-22 20:53:49 +0300

    first commit

---
+++ lec.py
@@ -0,0 +1,11 @@
+value = None
+a = 123
+b = 1.23
+#print(type(a))
+#print(type(b)) 
+value = 12345
+#print(type(value))
+s = 'hello world'
+#print(a,'-',b,'-',s)
+#print('{} - {} - {}'.format(a,b,s))
+print(f'{a} - {b} - {s}')
